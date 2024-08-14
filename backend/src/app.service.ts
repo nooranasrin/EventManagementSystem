66dec78213609ba68dd1442b9f1a71ad28c7fe41
Noora Nasrin Vc <nooranasrinvc@Nooras-MacBook-Pro.local>
2024-08-14 08:10:32 +0530

    Create a separate Prisma module and integrate it into App module

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './database/db.service';
+import { User } from 'prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(private databaseService: DatabaseService) {}
+
+  getUsers(): Promise<User[]> {
+    return this.databaseService.user.findMany();
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
